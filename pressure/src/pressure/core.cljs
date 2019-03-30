0dc04d491080fde605f0222cce2ca9ac31dd6e1a
timlathy <tim.labushev@gmail.com>
2019-03-30 11:27:31 +0300

    fade out the previous function, not the current line — looks much better

--- pressure/src/pressure/core.cljs
+++ pressure/src/pressure/core.cljs
@@ -13,28 +13,33 @@
         (* a (Math/sin (* (- 1 (/ r R)) t)))))])
 
 (def spirograph-functions
-  [(partial spirograph 5 4 3 40 40)
-   (partial spirograph 5 1 1 40 40)])
+  [(partial spirograph 5 4 1.2 40 40)
+   (partial spirograph 5 1 1 40 40)
+   (partial spirograph 5 1 2 40 40)])
 
 (def ^:const max-t (* 10 Math/PI))
-(def ^:const t-trail-len (* 5 Math/PI))
 (def ^:const dt 0.02)
-(def ^:const dt-steps-trail (/ t-trail-len dt))
-(def ^:const dalpha (/ 255 dt-steps-trail))
+(def ^:const prev-fn-decay-rate 1.1)
 
 (defn setup []
-  (q/frame-rate 60)
+  (q/frame-rate 30)
   (q/color-mode :hsb 360 100 100)
   ; initial state
   {:t 0
-   :fn-index 0})
+   :fn-index 0
+   :prev-fn-alpha 255
+   :prev-fn-index -1})
 
 (defn update-state [state]
   (if (< (:t state) max-t)
-    (update state :t + 0.1)
+    (-> state
+      (update :t + 1.2)
+      (update :prev-fn-alpha (fn [a] (- a (max 1 (* 0.2 a))))))
     (-> state
       (assoc :t 0)
-      (update :fn-index (fn [i] (mod (inc i) (count spirograph-functions)))))))
+      (assoc :prev-fn-alpha 255)
+      (update :fn-index (fn [i] (mod (inc i) (count spirograph-functions))))
+      (update :prev-fn-index (fn [i] (mod (inc i) (count spirograph-functions)))))))
 
 (defn point-pairs-with-step [f from to step]
   (->> (range from to step) (map f) (partition 2 1)))
@@ -47,14 +52,18 @@
   (q/with-translation [(/ (q/width) 2)
                        (/ (q/height) 2)]
     (let [t (:t state)
-          from-t (max 0 (- t t-trail-len))
           spiro-fn (nth spirograph-functions (:fn-index state))
-          point-pairs (point-pairs-with-step spiro-fn from-t t dt)
-          dt-steps (/ (min t t-trail-len) dt)
-          start-alpha (* dalpha (- dt-steps-trail dt-steps))]
-      (doseq [[i pts] (map-indexed vector point-pairs)]
-        (q/stroke 180 35 98 (+ start-alpha (* i dalpha)))
-        (apply q/line pts)))))
+          point-pairs (point-pairs-with-step spiro-fn 0 t dt)]
+      (q/stroke 180 35 98)
+      (doseq [pts point-pairs]
+        (apply q/line pts)))
+    (if (not= -1 (state :prev-fn-index))
+      (let [prev-fn (nth spirograph-functions (:prev-fn-index state))
+            alpha (:prev-fn-alpha state)]
+        (if (> alpha 0)
+          ((q/stroke 180 35 98 alpha)
+           (doseq [pts (point-pairs-with-step prev-fn 0 max-t dt)]
+             (apply q/line pts))))))))
 
 (defn ^:export run-sketch []
   (q/defsketch pressure
